3234274d90d242102425e8f99f1484a775abdc10
vitalinarh <uc2017255810@student.uc.pt>
2020-11-07 10:27:44 +0000

    Recurrent Network

--- training.m
+++ training.m
@@ -9,11 +9,13 @@ function nn = training(P_train, T_train, type, neurons, is_prediction)
         net.divideParam.trainRatio = 0.90;
         net.divideParam.valRatio = 0.10;
         net.divideParam.testRatio = 0; 
+        net.trainFcn = 'trainb';
         net.trainFcn = 'traingd';
         net.trainParam.epochs = 1000;
         nn = train(net, P_train, T_train);
     elseif strcmp(type, "Recurrent")
         disp("Recurrent")
+        net.trainFcn = 'trainb';
         net = layrecnet(1 : 2, neurons, "traingd");
         net.divideFcn = 'divideind';
         [net.divideParam.trainInd, ~, ~] = divideind(length(P_train), 1:length(P_train), [],[]);
