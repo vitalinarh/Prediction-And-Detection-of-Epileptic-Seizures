820faf7a51e75d622bf2772bc800237134f9828b
vitalinarh <uc2017255810@student.uc.pt>
2020-11-07 09:55:54 +0000

    Recurrent Network

--- training.m
+++ training.m
@@ -2,18 +2,21 @@ function nn = training(P_train, T_train, type, neurons, is_prediction)
     
     %% Configure Network
     
-    if strcmp(type, "Multilayer") == 0
+    if strcmp(type, "Multilayer")
         net = feedforwardnet(neurons);
-    else
-        net = feedforwardnet(neurons);
+        net.divideFcn = 'divideblock';
+        net.divideParam.trainRatio = 0.90;
+        net.divideParam.valRatio = 0.10;
+        net.divideParam.testRatio = 0; 
+        net.trainFcn = 'traingd';
+        net.trainParam.epochs = 1000;
+        nn = train(net, P_train, T_train);
+    elseif strcmp(type, "Recurrent")
+        disp("Recurrent")
+        net = layrecnet(1 : 2, neurons, "traingd");
+        net.divideFcn = 'divideind';
+        [net.divideParam.trainInd, ~, ~] = divideind(length(P_train), 1:length(P_train), [],[]);
+        net.trainParam.epochs = 1000;
+        nn = train(net, P_train, T_train, [], [], 'useParallel','no', 'useGPU', 'no', 'showResources', 'no');
     end
-    
-    net.divideFcn = 'divideblock';
-    net.divideParam.trainRatio = 0.90;
-    net.divideParam.valRatio = 0.10;
-    net.divideParam.testRatio = 0; 
-    net.trainFcn = 'traingd';
-    net.trainParam.epochs = 1000;
-    nn = train(net, P_train, T_train);
-    
 end
