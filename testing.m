ce0339f5f3e30b3a59d8fe26314a214050ad334f
vitalinarh <uc2017255810@student.uc.pt>
2020-11-08 14:01:30 +0000

    Added weight error for specilization

--- testing.m
+++ testing.m
@@ -4,7 +4,7 @@ function [prediction_results, detection_results]  = testing(nn, P_test, T_test, 
     
     prediction_results.true_positives = 0;
     prediction_results.true_negatives = 0;
-    prediction_results.false_positivess = 0;
+    prediction_results.false_positives = 0;
     prediction_results.false_negatives = 0;
     prediction_results.sensitivity = 0;
     prediction_results.specificity = 0;
@@ -12,7 +12,7 @@ function [prediction_results, detection_results]  = testing(nn, P_test, T_test, 
     
     detection_results.true_positives = 0;
     detection_results.true_negatives = 0;
-    detection_results.false_positivess = 0;
+    detection_results.false_positives = 0;
     detection_results.false_negatives = 0;
     detection_results.sensitivity = 0;
     detection_results.specificity = 0;
