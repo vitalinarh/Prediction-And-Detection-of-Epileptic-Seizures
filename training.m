651c8665f359275244f0c9a060aae3e1352496fb
vitalinarh <uc2017255810@student.uc.pt>
2020-11-12 12:23:33 +0000

    Error fixes

--- training.m
+++ training.m
@@ -16,7 +16,7 @@ function nn = training(P_train, T_train, type, neurons, is_prediction, spec, cla
     % Set up network error specialization for prediction, Preictal (2)
     if is_prediction 
         if strcmp(class_balancing, 'Off')
-            error = (interictal_total / preictal_total) * 1.5;
+            error = (interictal_total / preictal_total) * 5;
         else
             error = (interictal_total / preictal_total);
         end 
@@ -27,7 +27,7 @@ function nn = training(P_train, T_train, type, neurons, is_prediction, spec, cla
             error = 1;
         else
             if strcmp(class_balancing, 'Off')
-                error = (interictal_total / ictal_total) * 1.5;
+                error = (interictal_total / ictal_total) * 5;
             else
                 error = (interictal_total / ictal_total);
             end
@@ -57,5 +57,54 @@ function nn = training(P_train, T_train, type, neurons, is_prediction, spec, cla
         net.divideParam.testRatio = 0;
         net.trainParam.epochs = 1000;
         nn = train(net, P_train, T_train, [], [], 'useParallel', 'yes', 'useGPU', 'yes', 'showResources', 'no');
+        
+    elseif strcmp(type, "LSTM")
+         
+        % Set up the cell arrays for training
+        
+        % create a 1-by-length(P_train) cell array, where each cell contains a 29-by-1 column of P_train.
+        P_train = num2cell(P_train, 1);
+        
+        class1Ind = T_train(1, :) == 1;
+        class2Ind = T_train(2, :) == 1;
+        class3Ind = T_train(3, :) == 1;
+        class4Ind = T_train(4, :) == 1;
+        T_train = zeros(1, length(T_train)); 
+        T_train(class1Ind) = 1;
+        T_train(class2Ind) = 2;
+        T_train(class3Ind) = 3;
+        T_train(class4Ind) = 4;
+        
+        % T_train is a categorical vector of labels.
+        T_train = categorical(T_train)';
+       
+        % Set up the Network
+        numFeatures = 29;
+        numHiddenUnits = 100;
+        numClasses = 4;
+        miniBatchSize = 27;
+        maxEpochs = 20;
+        
+        layers = [ ...
+            sequenceInputLayer(numFeatures)
+            lstmLayer(numHiddenUnits, 'OutputMode', 'last')
+            fullyConnectedLayer(numClasses)
+            softmaxLayer
+            classificationLayer];
+        
+        options = trainingOptions('adam', ...
+                                'ExecutionEnvironment', 'cpu', ...
+                                'GradientThreshold', 1, ...
+                                'MaxEpochs', maxEpochs, ...
+                                'MiniBatchSize', miniBatchSize, ...
+                                'SequenceLength', 'longest', ...
+                                'Shuffle','never', ...
+                                'Verbose', 0, ...
+                                'Plots', 'training-progress');
+                            
+        nn = trainNetwork(P_train, T_train, layers, options); 
+    else
+        disp("CNN")
+
     end
 end
